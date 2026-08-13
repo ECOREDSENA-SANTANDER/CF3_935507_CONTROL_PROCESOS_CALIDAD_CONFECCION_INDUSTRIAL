fa91ee3812ac1fcf2b7194a4882975f2b66e13d4
PABG1990 <pedro19908454@gmail.com>
2026-08-13 11:18:51 -0500

    Act

--- src/config/global.js
+++ src/config/global.js
@@ -209,7 +209,7 @@ export default {
         {
           nombre: 'Claudia Johanna Gómez Pérez',
           cargo:
-            'Profesional G06. Responsable Ecosistema de Recursos Educativos Digitales (RED)',
+            'Profesional G06. Responsable Ecosistema Virtual de Recursos Educativos Digitales',
           centro: 'Centro Agroturístico - Regional Santander',
         },
         {
